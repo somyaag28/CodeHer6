0713def5d0ee27c30af626c9cbc7bfdedec73c27
Saanch Thakur <maryashliegh@gmail.com>
2026-08-23 01:04:06 +0530

    Update home endpoint message and add identify endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from matching import match_image
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello CodeHer6!"}
+    return {"message": "CodeHer6 API is running"}
+
+
+@app.post("/identify")
+def identify():
+    product = match_image()
+    return product
