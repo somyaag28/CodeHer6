cdc18c42602494c759c99e17ea18fad086f7721a
sanviiuuuuuu <sanvinagpal09@gmail.com>
2026-08-23 00:42:59 +0530

    Add product function

--- database/inventory.py
+++ database/inventory.py
@@ -19,5 +19,18 @@ def create_database():
     conn.commit()
     conn.close()
 
+def add_product(product_id, name, price, stock):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
 
+    cursor.execute(
+        """
+        INSERT INTO inventory (id, name, price, stock)
+        VALUES (?, ?, ?, ?)
+        """,
+        (product_id, name, price, stock)
+    )
+
+    conn.commit()
+    conn.close()
 create_database()
